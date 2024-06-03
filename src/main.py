654bc6568b4c47d010dfaacee13cc867a894d414
bugston <bugstonn@gmail.com>
2024-06-03 14:22:44 +0300

    validating data now working when giving post request

--- src/main.py
+++ src/main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from datetime import datetime
 import redis
+from pydantic import constr, conint, Field
 from json import JSONDecodeError
+from typing import Annotated
+from typing import Optional
 
 app = FastAPI()
 
@@ -18,20 +21,25 @@ def read_root():
     return {"number of hits": r.get("hits")}
 
 
+
+
 class Item(BaseModel):
-    name: str = None
-    description: str = None
-    price: float = None
-    tax: float = None
+    name: str = Field(max_length=10, pattern=r'^[a-zA-Z]+$')
+    description: str = Field(max_length=50, pattern=r'^[a-zA-Z]+$')
+    price: float = Field(conint(gt=0))
+    tax: Optional[float] = None
+
 
 
 items_storage = []
+
 
 def save_item(item_data):
     items_storage.append(item_data)
 
 @app.post("/items/")
 async def create_items(items: list[Item]):
+    print(items_storage)
     try:
         r.incr("hits")
 
@@ -39,7 +47,7 @@ async def create_items(items: list[Item]):
 
         items_data = []
         for item in items:
-            item_data = item.dict()
+            item_data = item.model_dump()
             item_data["timestamp"] = timestamp
             items_data.append(item_data)
         
@@ -51,6 +59,7 @@ async def create_items(items: list[Item]):
     
 @app.get("/items/")
 async def get_items():
+    print(items_storage)
     return {"items": items_storage}
 
 
@@ -65,7 +74,7 @@ async def clear_prices():
     for item in items_storage:
         item["price"] = None
 
-@app.get("/items/")
+@app.get("/items/sorted")
 async def sorted():
     global items_storage
     for item in items_storage:
